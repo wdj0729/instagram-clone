6ca7413a1a973f975d89528374e71f777a84e3c4
wdj0729 <wdj930729@naver.com>
2021-01-26 15:19:39 +0900

    #1. 메인화면

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+/* eslint-disable */
+import React, { useState } from 'react';
+import logo from './image.jpg';
+import logo2 from './image2.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="leftBox">
+          <img src={logo}></img>
+        </div>
+        <div className="rightBox">
+          <div className="loginBox">
+            <img src={logo2}></img><br></br><br></br>
+            <form>
+              <input placeholder="이메일"></input><br></br><br></br>
+              <input placeholder="비밀번호"></input>
+            </form><br></br>
+            <button>로그인</button><br></br><br></br>
+            <a href="/">비밀번호를 잊으셨나요?</a>
+          </div>
+          <br></br>
+          <div className="signupBox">
+            <p>계정이 없으신가요? <a href="/">가입하기</a></p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
